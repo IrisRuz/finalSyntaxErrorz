e0a9567f4d8e3b7a95f6980eaa08931154ae825e
itsyourGOAT <123419631+itsyourGOAT@users.noreply.github.com>
2023-12-07 13:47:22 -0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,9 @@
+'''
+CS3250 - Software Development Methods and Tools - Project 3 Final
+Team:SyntaxErrorz
+Description: Project 3 User Task Management
+'''
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, SubmitField, validators, EmailField, DateField
 from wtforms.validators import DataRequired, InputRequired, Length
