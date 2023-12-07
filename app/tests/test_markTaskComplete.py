32c81ce19007b72aa27cbcf5078adbfc95c034ec
Iris Ruz <iruz@msudenver.edu>
2023-12-07 09:23:40 -0700

    corrected marrk test complete test

--- app/tests/test_markTaskComplete.py
+++ app/tests/test_markTaskComplete.py
@@ -2,6 +2,8 @@ import unittest
 from app import app, db
 from app.models import User, Task
 from flask_login import login_user
+import bcrypt
+from bs4 import BeautifulSoup
 
 class TaskRouteTestCase(unittest.TestCase):
 
@@ -11,21 +13,52 @@ class TaskRouteTestCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
 
-        test_user = User(id='testUser2005',email='testemail7@example.com',password='testpassword1')
-        test_task = Task(id =895, user_id=test_user.id, title='Test Task Title', description='test description',completed= False )
-        db.session.add(test_user)
-        db.session.add(test_task)
+        salt = bcrypt.gensalt()
+        password = 'testpassword'.encode('utf-8')
+        hashed_password = bcrypt.hashpw(password, salt)
+        self.test_user = User(id='testuser',email='testemail@example.com',password=hashed_password)
+        db.session.add(self.test_user)
         db.session.commit()
-    
+
+        login_page_response = self.app.get('/users_signin')
+        soup = BeautifulSoup(login_page_response.data, 'html.parser')
+        csrf_token = soup.find('input', id='csrf_token')['value']
+
+
+        login_response = self.app.post('/users_signin', data={
+            'id': 'testuser',  
+            'password': 'testpassword',
+            'csrf_token': csrf_token
+        }, follow_redirects=True)
+        print("login response:", login_response.data)
+        print("Login response status code:", login_response.status_code)
+        print("Login response data:", login_response.data.decode('utf-8'))
+
+        self.test_task = Task(id =1, user_id=self.test_user.id, title='Test Task Title', description='test description',completed= False )
+        db.session.add(self.test_task)
+        db.session.commit()
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_mark_task_complete(self):
-        response = self.app.post('/tasks/mark_complete/895', follow_redirects=True)
-        test_task = db.session.query(Task).filter_by(id=895).first()
-        self.assertTrue(test_task.completed)
+        print("Initial Task Completed Status:", self.test_task.completed)
+        form_data = {'completed': 'on'}
+        response = self.app.post(f'/tasks/mark_complete/{self.test_task.id}', 
+                             data=form_data, follow_redirects=True)
+        
+        updated_task = Task.query.filter_by(id=self.test_task.id).first()
+        print("Form Data:", form_data)
+        print("Response Status Code:", response.status_code)
+        print("Updated Task ID:", updated_task.id)
+        print("Updated Task Completed Status:", updated_task.completed)
+        print("Test User ID:", self.test_user.id)
+        print("Task User ID:", updated_task.user_id)
+        print("Response Data:", response.data)
+
+        self.assertTrue(updated_task.completed, "Task was not marked as completed")
         self.assertIn(b'Task status updated.', response.data)
 
 if __name__ == '__main__':
